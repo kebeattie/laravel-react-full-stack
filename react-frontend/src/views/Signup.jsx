dae66ce2d5c3893bb698f59294f773d1cd43620d
kebeattie <kebeattie98@hotmail.com>
2024-10-05 15:37:43 +0100

    Added axios-client to make post requests from sign up form

--- react-frontend/src/views/Signup.jsx
+++ react-frontend/src/views/Signup.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import {useRef} from 'react'
 import { Link } from 'react-router-dom';
+import axiosClient from '../axios-client';
+import { useStateContext } from '../contexts/ContextProvider'
 
 const Signup = () => {
 
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+  const {setUser, setToken} = useStateContext();
+
+
   const onSubmit = (e) => {
     e.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value, 
+    };
+
+    
+
+    axiosClient.post('/signup', payload)
+      .then(({data}) => {
+        setToken(data.token);
+        setUser(data.user);
+      })
+      .catch(err => {
+        const response = err.response;
+        if(response && response.status == 422) {
+          console.log(response.data.errors);
+        }
+      })
   }
 
   return (
@@ -14,10 +44,10 @@ const Signup = () => {
           <h1 className='title'>
             Create an account
           </h1>
-        <input type="text" placeholder='Full Name' required />
-          <input type="email" placeholder='Email Address' required />
-          <input type="password" placeholder='Password' required />
-          <input type="password" placeholder='Password Confirmation' required />
+        <input ref={nameRef} type="text" placeholder='Full Name' required />
+          <input ref={emailRef} type="email" placeholder='Email Address' required />
+          <input ref={passwordRef} type="password" placeholder='Password' required />
+          <input ref={passwordConfirmationRef} type="password" placeholder='Password Confirmation' required />
           <button className='btn btn-block'>Sign Up</button>
           <p className='message'>
             Already Registered? <Link to='/login'>Sign in</Link>
